0d8c29503bc89a51416f64b703846de24da6618b
Syed Vilayat Ali Rizvi <vilayatcodemysite@gmail.com>
2022-06-18 17:11:44 +0530

    Added basic skeleton of the frontend

--- src/components/Cards/CartCard.jsx
+++ src/components/Cards/CartCard.jsx
@@ -1,5 +1,91 @@
-const CartCard = () => {
-  return <div>CartCard</div>;
+// importing react hook
+import { useEffect } from "react";
+import { useState } from "react";
+
+const CartCard = ({ img, name, price, q }) => {
+  // states
+  const [quantity, setQuantity] = useState(0);
+  const [product, setProduct] = useState([]);
+
+  // useEffect hook
+  useEffect(() => setQuantity(q), []);
+
+  // incr and decr
+  const incrementQuantity = () => setQuantity(quantity + 1);
+  const decrementQuantity = () => {
+    if (quantity > 1) setQuantity(quantity - 1);
+  };
+  const removeProduct = () => {};
+  return (
+    <div className="card" style={{ width: "90vw", margin: "2vw auto" }}>
+      <div
+        className="card-horizontal"
+        style={{
+          display: "flex",
+          flex: "1 1 auto",
+        }}
+      >
+        <div
+          className="img-square-wrapper"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            className="container img-fluid"
+            src={img}
+            alt="Card image cap"
+            style={{
+              width: "22rem",
+              height: "auto",
+              objectFit: "contain",
+            }}
+          />
+        </div>
+        <div className="card-body">
+          <h4 className="card-title">
+            <b>Product: </b>
+            {name}
+          </h4>
+          <hr />
+          <p className="card-text">
+            <b>Quantity: </b>
+            {quantity}
+          </p>
+          <p className="card-text">
+            <b>Total: $ </b>
+            <i className="text-success h6">{quantity * price}</i>
+          </p>
+          <hr />
+          <div className="row">
+            <button
+              className="col-2 btn btn-success"
+              onClick={incrementQuantity}
+            >
+              Add 1 more
+            </button>
+            <div className="col-1"></div>
+            <button
+              className="col-2 btn btn-warning"
+              onClick={decrementQuantity}
+            >
+              Remove 1 more
+            </button>
+            <div className="col-1"></div>
+            <button className="col-2 btn btn-danger" onClick={removeProduct}>
+              Remove
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="card-footer">
+        <small className="text-muted">Added about a minute ago</small>
+      </div>
+    </div>
+  );
 };
 
 export default CartCard;
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,43 @@
 // import components
-import Product from "../components/Cards/Product";
+import CartCard from "../components/Cards/CartCard";
+import Img1 from "../assets/img/bg-showcase-1.jpg";
+
+// sample data
+const productInCart = [
+  {
+    name: "Product - 1",
+    description: "lorem10asdafasfkdakhad afdsfs gfd g",
+    q: 2,
+    ppu: 12,
+  },
+  {
+    name: "Product - 2",
+    description: "lorem10asdafasfkdakhad afdsfs gfd g",
+    q: 3,
+    ppu: 12,
+  },
+];
 
 const Cart = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 mt-3">
+            {productInCart.map((product, index) => (
+              <div className="col-12">
+                <CartCard
+                  img={Img1}
+                  name={product.name}
+                  description={product.description}
+                  price={product.ppu}
+                  q={product.q}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
